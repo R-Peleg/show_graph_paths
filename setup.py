eeb421f15c0134ea89282bd33739b39119d99dbd
Reuven Peleg <4018286+R-Peleg@users.noreply.github.com>
2024-07-01 10:48:14 +0300

    fix setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='show_graph_paths',
-  version='0.1.0',
+  version='0.1.1',
   packages=find_packages(where='show_graph_paths'),
   include_package_data=True,
   description='Extracts subgraph based on regex filters from nx DiGraphs',
